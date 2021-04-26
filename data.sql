1da52158db1be62d6f8abe096d17a6cd1631816e
Max Goeke <4ked@users.noreply.github.com>
2021-04-26 02:03:28 -0400

    data insert

---
+++ data.sql
@@ -0,0 +1,367 @@
+INSERT INTO team VALUES 
+    ('Fairly University','College Park','Maryland','Smith');
+INSERT INTO team VALUES 
+    ('Popper University','Bryn Mawr','Pennsylvania','Henderson');
+INSERT INTO team VALUES 
+    ('University of Lincoln','St.Louis','Missouri','Lindmark');
+INSERT INTO team VALUES
+    ('Greenworld University','Atlanta','Georgia','Plank');
+INSERT INTO team VALUES
+    ('University of Killwood','Tampa','Florida','McGreggor');
+INSERT INTO team VALUES
+    ('Stockton University','Princeton','New Jersey','Haskel');
+INSERT INTO team VALUES
+    ('Derrickville University','Los Angelos','California','Ruth');
+INSERT INTO team VALUES
+    ('Fisher University','New York City','New York','Green');
+ 
+--
+INSERT INTO player VALUES
+    (1,'Matt','Fern', 0, 'Guard', 'Senior', 'Fairly University');
+INSERT INTO player VALUES
+    (2,'Dejuan','Jumbo', 17, 'Center', 'Senior', 'Fairly University');
+INSERT INTO player VALUES
+    (3,'Mike','Johnson', 32, 'Forward', 'Junior', 'Fairly University' );
+INSERT INTO player VALUES
+    (4,'KeVon','Fires', 22, 'Center', 'Junior', 'Fairly University' );
+INSERT INTO player VALUES
+    (5,'Jermaine','Kilman', 26, 'Forward', 'Junior', 'Fairly University' );
+INSERT INTO player VALUES
+    (6,'Walker','Smith', 13, 'Guard', 'Sophomore', 'Fairly University' );
+INSERT INTO player VALUES
+    (7,'Liam','Lowry', 43, 'Center', 'Sophomore', 'Fairly University' );
+INSERT INTO player VALUES
+    (8,'Corey','Kicker', 11, 'Center', 'Sophomore', 'Fairly University' );
+INSERT INTO player VALUES
+    (9,'Collin','Preston', 10, 'Forward', 'Freshman', 'Fairly University' );
+INSERT INTO player VALUES
+    (10,'Michael','Scott', 99, 'Guard', 'Freshman', 'Fairly University' );
+
+--
+INSERT INTO player VALUES
+    (11,'Roger','Drummer', 2, 'Guard', 'Senior', 'Popper University');
+INSERT INTO player VALUES
+    (12,'Justin','Hero', 4, 'Center', 'Senior', 'Popper University');
+INSERT INTO player VALUES
+    (13,'Rich','Lopper', 6, 'Forward', 'Junior', 'Popper University' );
+INSERT INTO player VALUES
+    (14,'Dez','Len',8, 'Center', 'Junior', 'Popper University' );
+INSERT INTO player VALUES
+    (15,'Alex','Trimble',10, 'Forward', 'Junior', 'Popper University' );
+INSERT INTO player VALUES
+    (16,'Melo','Morin',12, 'Guard', 'Sophomore', 'Popper University' );
+INSERT INTO player VALUES
+    (17,'John','Jordan',14, 'Center', 'Sophomore', 'Popper University' );
+INSERT INTO player VALUES
+    (18,'Michael','Order',16, 'Center', 'Sophomore', 'Popper University' );
+INSERT INTO player VALUES
+    (19,'Shawn','Larow',18, 'Forward', 'Freshman', 'Popper University' );
+INSERT INTO player VALUES
+    (20,'Austin','Gormley',20, 'Guard', 'Freshman', 'Popper University' );
+    
+--
+INSERT INTO player VALUES
+    (21,'Armand','Fraiser',23, 'Guard', 'Senior', 'University of Lincoln');
+INSERT INTO player VALUES
+    (22,'Kris','Otto',24, 'Center', 'Senior', 'University of Lincoln');
+INSERT INTO player VALUES
+    (23,'Ken','Jenkins',25, 'Forward', 'Junior', 'University of Lincoln' );
+INSERT INTO player VALUES
+    (24,'Ryan','Stokes',26, 'Center', 'Junior', 'University of Lincoln' );
+INSERT INTO player VALUES
+    (25,'Justin','Simmons',27, 'Forward', 'Junior', 'University of Lincoln' );
+INSERT INTO player VALUES
+    (26,'Kyrie','Loft',28, 'Guard', 'Sophomore', 'University of Lincoln' );
+INSERT INTO player VALUES
+    (27,'Walker','Tim',29, 'Center', 'Sophomore', 'University of Lincoln' );
+INSERT INTO player VALUES
+    (28,'Brian','Mint',30, 'Center', 'Sophomore', 'University of Lincoln' );
+INSERT INTO player VALUES
+    (29,'Paul','Gerlach',31, 'Forward', 'Freshman', 'University of Lincoln' );
+INSERT INTO player VALUES
+    (30,'Nick','Turner',32, 'Guard', 'Freshman', 'University of Lincoln' );
+
+--
+INSERT INTO player VALUES
+    (31,'Paul','Jones',34, 'Guard', 'Senior', 'Greenworld University');
+INSERT INTO player VALUES
+    (32,'Henry','Archidianco',32, 'Center', 'Senior', 'Greenworld University');
+INSERT INTO player VALUES
+    (33,'Frank','Sister',65, 'Forward', 'Junior', 'Greenworld University' );
+INSERT INTO player VALUES
+    (34,'Kris','Flister',3, 'Center', 'Junior', 'Greenworld University' );
+INSERT INTO player VALUES
+    (35,'Jeramiah','Plow',24, 'Forward', 'Junior', 'Greenworld University' );
+INSERT INTO player VALUES
+    (36,'Matt','Greggor',16, 'Guard', 'Sophomore', 'Greenworld University' );
+INSERT INTO player VALUES
+    (37,'Rick','Townsend',25, 'Center', 'Sophomore', 'Greenworld University' );
+INSERT INTO player VALUES
+    (38,'Monov','Sherb',13, 'Center', 'Sophomore', 'Greenworld University' );
+INSERT INTO player VALUES
+    (39,'Nick','Totter',4, 'Forward', 'Freshman', 'Greenworld University' );
+INSERT INTO player VALUES
+    (40,'Derrick','Reynolds',65, 'Guard', 'Freshman', 'Greenworld University' );
+    
+--
+INSERT INTO player VALUES
+    (41,'Justin','Moorre',54, 'Guard', 'Senior', 'University of Killwood');
+INSERT INTO player VALUES
+    (42,'Greg','McKenna',53, 'Center', 'Senior', 'University of Killwood');
+INSERT INTO player VALUES
+    (43,'Michael','Jordan',2, 'Forward', 'Junior', 'University of Killwood' );
+INSERT INTO player VALUES
+    (44,'Willaim','Flames',4, 'Center', 'Junior', 'University of Killwood' );
+INSERT INTO player VALUES
+    (45,'Earl','Mostum',23, 'Forward', 'Junior', 'University of Killwood' );
+INSERT INTO player VALUES
+    (46,'Reggie','William',45, 'Guard', 'Sophomore', 'University of Killwood' );
+INSERT INTO player VALUES
+    (47,'Harry','Lowry',10, 'Center', 'Sophomore', 'University of Killwood' );
+INSERT INTO player VALUES
+    (48,'Sam','Sweeney',7, 'Center', 'Sophomore', 'University of Killwood' );
+INSERT INTO player VALUES
+    (49,'Scott','Mackay',8, 'Forward', 'Freshman', 'University of Killwood');
+INSERT INTO player VALUES
+    (50,'Andrew','Willis',9, 'Guard', 'Freshman', 'University of Killwood' );
+
+--
+INSERT INTO player VALUES
+    (51,'Dylan','Lopresti', 3, 'Guard', 'Senior', 'Stockton University');
+INSERT INTO player VALUES
+    (52,'Fred','Muster', 2, 'Center', 'Senior', 'Stockton University');
+INSERT INTO player VALUES
+    (53,'Jack','Zest', 4, 'Forward', 'Junior', 'Stockton University' );
+INSERT INTO player VALUES
+    (54,'Tom','Sawyer', 65, 'Center', 'Junior', 'Stockton University' );
+INSERT INTO player VALUES
+    (55,'Jordan','History',8, 'Forward', 'Junior', 'Stockton University' );
+INSERT INTO player VALUES
+    (56,'Walker','Smith',34, 'Guard', 'Sophomore', 'Stockton University' );
+INSERT INTO player VALUES
+    (57,'Liam','Lowry',11, 'Center', 'Sophomore', 'Stockton University' );
+INSERT INTO player VALUES
+    (58,'Corey','Kicker',9, 'Center', 'Sophomore', 'Stockton University' );
+INSERT INTO player VALUES
+    (59,'Justin','Crocker',10, 'Forward', 'Freshman', 'Stockton University' );
+INSERT INTO player VALUES
+    (60,'Peter','Thabit',15, 'Guard', 'Freshman', 'Stockton University' );
+
+--
+INSERT INTO player VALUES
+    (61,'Aiden','Bell',2, 'Guard', 'Senior', 'Derrickville University');
+INSERT INTO player VALUES
+    (62,'Corey','Son',4, 'Center', 'Senior', 'Derrickville University');
+INSERT INTO player VALUES
+    (63,'Matt','James',5, 'Forward', 'Junior', 'Derrickville University' );
+INSERT INTO player VALUES
+    (64,'Derrick','Poolston',6, 'Center', 'Junior', 'Derrickville University' );
+INSERT INTO player VALUES
+    (65,'Aaron','Rafferty',10, 'Forward', 'Junior', 'Derrickville University');
+INSERT INTO player VALUES
+    (66,'Michael','Swanson',11, 'Guard', 'Sophomore', 'Derrickville University');
+INSERT INTO player VALUES
+    (67,'Austin','Pietro',13, 'Center', 'Sophomore', 'Derrickville University');
+INSERT INTO player VALUES
+    (68,'Stanley','Kells',14, 'Center', 'Sophomore', 'Derrickville University');
+INSERT INTO player VALUES
+    (69,'Jude','Baker',16, 'Forward', 'Freshman', 'Derrickville University');
+INSERT INTO player VALUES
+    (70,'Preacher','Johnson',19, 'Guard', 'Freshman', 'Derrickville University' );
+    
+--
+INSERT INTO player VALUES
+    (71,'Liam','Gutter',9, 'Guard', 'Senior', 'Fisher University');
+INSERT INTO player VALUES
+    (72,'Keith','Samuels',55, 'Center', 'Senior', 'Fisher University');
+INSERT INTO player VALUES
+    (73,'Will','Turgeon',66, 'Forward', 'Junior', 'Fisher University' );
+INSERT INTO player VALUES
+    (74,'Terrin','Toner',77, 'Center', 'Junior', 'Fisher University' );
+INSERT INTO player VALUES
+    (75,'Lewis','Bentley',88, 'Forward', 'Junior', 'Fisher University' );
+INSERT INTO player VALUES
+    (76,'Greg','Gannon',99, 'Guard', 'Sophomore', 'Fisher University' );
+INSERT INTO player VALUES
+    (77,'Alec','Bent',3, 'Center', 'Sophomore', 'Fisher University' );
+INSERT INTO player VALUES
+    (78,'Reed','Moshedi',44, 'Center', 'Sophomore', 'Fisher University' );
+INSERT INTO player VALUES
+    (79,'Parker','Bagshaw',33, 'Forward', 'Freshman', 'Fisher University' );
+INSERT INTO player VALUES
+    (80,'Andrew','Ramsayer',22, 'Guard', 'Freshman', 'Fisher University' );
+    
+--Statistics 
+
+INSERT INTO statistics VALUES
+    (1,543,54,89, 43);
+INSERT INTO statistics VALUES
+    (2,1004,500,200, 62);
+INSERT INTO statistics VALUES
+    (3,650,430,223, 47);
+INSERT INTO statistics VALUES
+    (4,412,250,216, 41);
+INSERT INTO statistics VALUES
+    (5, 516, 346, 287, 51);
+INSERT INTO statistics VALUES
+    (6, 123, 57, 86, 37);
+INSERT INTO statistics VALUES
+    (7, 167, 62, 76, 36);
+INSERT INTO statistics VALUES
+    (8, 120, 43, 53, 38);
+INSERT INTO statistics VALUES
+    (9, 65, 12, 24, 32);
+INSERT INTO statistics VALUES
+    (10, 43, 9, 13, 29);
+--
+INSERT INTO statistics VALUES
+    (11, 989, 564, 476, 61);
+INSERT INTO statistics VALUES
+    (12, 876, 430, 452, 57);
+INSERT INTO statistics VALUES
+    (13, 789, 412, 345, 51);
+INSERT INTO statistics VALUES
+    (14, 902, 278, 345, 59);
+INSERT INTO statistics VALUES
+    (15, 678, 322, 287, 51);
+INSERT INTO statistics VALUES
+    (16, 502, 233, 257, 49);
+INSERT INTO statistics VALUES
+    (17, 492, 212, 196, 48);
+INSERT INTO statistics VALUES
+    (18, 120, 56, 43, 39);
+INSERT INTO statistics VALUES
+    (19, 98, 35, 36, 38);
+INSERT INTO statistics VALUES
+    (20, 54, 20, 23, 36);
+    
+--
+INSERT INTO statistics VALUES
+    (21, 1003, 435, 512, 62);
+INSERT INTO statistics VALUES
+    (22, 876, 497, 401, 59);
+INSERT INTO statistics VALUES
+    (23, 716, 376, 212, 54);
+INSERT INTO statistics VALUES
+    (24, 765, 387, 354, 56);
+INSERT INTO statistics VALUES
+    (25, 615, 312, 324, 52);
+INSERT INTO statistics VALUES
+    (26, 342, 157, 120, 42);
+INSERT INTO statistics VALUES
+    (27, 256, 122, 145, 45);
+INSERT INTO statistics VALUES
+    (28, 134, 60, 43, 38);
+INSERT INTO statistics VALUES
+    (29, 50, 10, 18, 36);
+INSERT INTO statistics VALUES
+    (30, 2, 0, 0, 20);
+
+--
+INSERT INTO statistics VALUES
+    (31,643,122,189, 46);
+INSERT INTO statistics VALUES
+    (32,1004,402,208, 67);
+INSERT INTO statistics VALUES
+    (33,657,430,223, 47);
+INSERT INTO statistics VALUES
+    (34,412,250,216, 41);
+INSERT INTO statistics VALUES
+    (35, 518, 341, 289, 51);
+INSERT INTO statistics VALUES
+    (36, 137, 52, 89, 39);
+INSERT INTO statistics VALUES
+    (37, 169, 61, 75, 36);
+INSERT INTO statistics VALUES
+    (38, 12, 4, 5, 29);
+INSERT INTO statistics VALUES
+    (39, 65, 12, 24, 32);
+INSERT INTO statistics VALUES
+    (40, 43, 9, 13, 29);
+    
+--
+INSERT INTO statistics VALUES
+    (41, 1231, 635, 412, 63);
+INSERT INTO statistics VALUES
+    (42, 676, 297, 301, 58);
+INSERT INTO statistics VALUES
+    (43, 719, 386, 214, 53);
+INSERT INTO statistics VALUES
+    (44, 765, 387, 354, 56);
+INSERT INTO statistics VALUES
+    (45, 512, 212, 364, 51);
+INSERT INTO statistics VALUES
+    (46, 345, 155, 89, 43);
+INSERT INTO statistics VALUES
+    (47, 257, 125, 148, 43);
+INSERT INTO statistics VALUES
+    (48, 136, 54, 41, 37);
+INSERT INTO statistics VALUES
+    (49, 52, 9, 18, 31);
+INSERT INTO statistics VALUES
+    (50, 4, 0, 1, 25);
+
+--
+INSERT INTO statistics VALUES
+    (51, 1176, 685, 423, 61);
+INSERT INTO statistics VALUES
+    (52, 776, 397, 306, 59);
+INSERT INTO statistics VALUES
+    (53, 819, 396, 254, 53);
+INSERT INTO statistics VALUES
+    (54, 765, 387, 352, 54);
+INSERT INTO statistics VALUES
+    (55, 612, 213, 361, 49);
+INSERT INTO statistics VALUES
+    (56, 346, 151, 96, 43);
+INSERT INTO statistics VALUES
+    (57, 257, 127, 149, 41);
+INSERT INTO statistics VALUES
+    (58, 132, 56, 42, 37);
+INSERT INTO statistics VALUES
+    (59, 52, 7, 12, 32);
+INSERT INTO statistics VALUES
+    (60, 5, 1, 1, 10);
+--
+INSERT INTO statistics VALUES
+    (61, 1103, 457, 503, 61);
+INSERT INTO statistics VALUES
+    (62, 1001, 472, 411, 58);
+INSERT INTO statistics VALUES
+    (63, 718, 362, 218, 55);
+INSERT INTO statistics VALUES
+    (64, 771, 371, 332, 52);
+INSERT INTO statistics VALUES
+    (65, 623, 318, 321, 53);
+INSERT INTO statistics VALUES
+    (66, 341, 128, 121, 41);
+INSERT INTO statistics VALUES
+    (67, 259, 126, 141, 43);
+INSERT INTO statistics VALUES
+    (68, 136, 61, 41, 39);
+INSERT INTO statistics VALUES
+    (69, 54, 11, 16, 37);
+INSERT INTO statistics VALUES
+    (70, 8, 2, 2, 25);
+    
+--
+INSERT INTO statistics VALUES
+    (71,643,154,159, 44);
+INSERT INTO statistics VALUES
+    (72,1134,476,202, 63);
+INSERT INTO statistics VALUES
+    (73,750,432,225, 48);
+INSERT INTO statistics VALUES
+    (74,414,253,219, 45);
+INSERT INTO statistics VALUES
+    (75, 608, 246, 267, 51);
+INSERT INTO statistics VALUES
+    (76, 153, 67, 85, 38);
+INSERT INTO statistics VALUES
+    (77, 177, 52, 74, 37);
+INSERT INTO statistics VALUES
+    (78, 121, 53, 63, 39);
+INSERT INTO statistics VALUES
+    (79, 45, 9, 13, 34);
+INSERT INTO statistics VALUES
+    (80, 14, 5, 4, 24);
